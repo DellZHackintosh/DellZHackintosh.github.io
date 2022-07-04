ef1d39ef5f7e6069790cdc477b51407e2d450de8
DellZHackintosh <77269678+DellZHackintosh@users.noreply.github.com>
2022-07-04 21:52:11 +0800

    加速⏩！

--- Wallpapers/Wallpaper.js
+++ Wallpapers/Wallpaper.js
@@ -1,3 +1,3 @@
 var num = Math.floor(Math.random() * 36) + 1;
-var photo = "./Wallpapers/Placeholder" + num + ".jpg"
+var photo = "https://raw.githubusercontents.com/DellZHackintosh/DellZHackintosh.github.io/gh-pages/Wallpapers/Placeholder" + num + ".jpg"
 document.body.style.backgroundImage="url("+[photo]+")";
